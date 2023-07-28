62b9f5f3864af96ca97b4221f0926d37e460d1e3
jzyzak <joshuazyzak@gmail.com>
2023-07-28 09:27:41 -0400

    fixes for network stream and functions

--- lane_detection.py
+++ lane_detection.py
@@ -1,59 +1,58 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from random import randrange
+import random
 
-# Problem 1
 
-def literallyEverything(img, threshold1=90, threshold2=100, apertSize=3, minLength=10, maxGap=60, color=(0,255,0)):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray, threshold1, threshold2, apertureSize=apertSize) 
-    lines = cv2.HoughLinesP(
-                    edges, #described above
-                    1, #1 pixel resolution parameter
-                    np.pi/180, # 1 degree resolution parameter
-                    60, #min number of intersections/votes
-                    minLineLength=minLength,
-                    maxLineGap=maxGap,
-            ) # detect lines
-    try:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(img, (x1, y1), (x2, y2), color, 2)
-            slope = (y2-y1)/(x2-x1)
-            print(str(slope))
-    except TypeError:
-        pass
+def detect_lines(img, threshold1 = 50, threshold2 = 150, apertureSize = 3, minLineLength = 100, maxLineGap = 10):
+    '''
     
-    return img
+    takes an image as an input and returns a list of detected lines
+    
+    parameters:
+        img: the image to process
+        threshold1: the first threshold for the Canny edge detector (default: 50)
+        threshold2: the second threshold for the Canny edge detector (default: 150)
+        apertureSize: the aperture size for the Sobel operator (default: 3)
+        minLineLength: the minimum length of a line (default: 100)
+        maxLineGap: the maximum gap between two points to be considered in the same line (default: 10)
+    
+    '''
 
-# Part 1
-def detect_lines(img, threshold1=50, threshold2=150, apertureSize=3, minLineLength=100, maxLineGap=10):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray, threshold1, threshold2, apertureSize)
-    #plt.imshow(edges)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to grayscale
+    edges = cv2.Canny(gray, threshold1, threshold2, apertureSize = apertureSize) # detect edges, gray is image in grayscale, 50 and 150 represent 2 images that have been 
+                                                      # threshholded at 2 different levels, apertureSize controls how much light the image gets and how exposed it is
     lines = cv2.HoughLinesP(
                     edges, #described above
                     rho = 1, #1 pixel resolution parameter
                     theta = np.pi/180, # 1 degree resolution parameter
                     threshold = 125, #min number of intersections/votes
-                    minLineLength=minLineLength,
-                    maxLineGap=maxLineGap,
+                    minLineLength = minLineLength,
+                    maxLineGap = maxLineGap,
             ) # detect lines
+
+
     return lines
 
-# Part 2
-def draw_lines(img, lines, color=(0,255,0)):
+def draw_lines(img, lines, color = (0,255,0)):
+    '''
+    takes an image and a list of lines as inputs and returns an image with the lines drawn on it
+
+    parameters:
+        img: the image to process
+        lines: the list of lines to draw
+        color: the color of the lines (default: (0, 255, 0))
+    
+    '''
     try:
         for line in lines:
             x1, y1, x2, y2 = line[0]
             cv2.line(img, (x1, y1), (x2, y2), color, 2)
     except TypeError:
         pass
-    #plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    #plt.show()
+    return img
 
-# Part 3
 def get_slopes_intercepts(lines):
     '''
     takes a list of lines as an input and returns a list of slopes and a list of intercepts
@@ -67,81 +66,78 @@ def get_slopes_intercepts(lines):
     intercepts = []
 
     for line in lines:
-        x1, y1, x2, y2 = line[0]
-        slope = (y2-y1)/(x2-x1)
-        slopes.append(slope)
-        intercept = ((((2160 - y1)/slope)  )+ x1)
-        intercepts.append(intercept)
+        try:
+            x1, y1, x2, y2 = line[0]
+            slope = (y2-y1)/(x2-x1)
+            slopes.append(slope)
+            intercept = ((((2160 - y1)/slope)  )+ x1)
+            intercepts.append(intercept)
+        
+        except ZeroDivisionError:
+            pass
 
     return slopes, intercepts
 
-# Part 4
 def detect_lanes(lines):
+
+    '''
+    takes a list of lines as an input and returns a list of lanes
+
+    parameters:
+        lines: the list of lines to process
+    
+    '''
+
     slopeList, xInterceptList = get_slopes_intercepts(lines)
-    #print (f"slopeList:{slopeList}")
-    #print (f"xInterceptList:{xInterceptList}")
+    # print (f"slopeList:{slopeList}")
+    # print (f"xInterceptList:{xInterceptList}")
     lanes = []
     #check of the lines intersect on the screen
     if len(slopeList)> 1:
         for i in range(0,len(slopeList)):
-            # if (len(slopeList) > 1):
-            #     i += 1
-            #     print("added i")
+           
             for j in range (i+1,len(slopeList)):
-                #print(f"DistREQ:{abs(xInterceptList[i]-xInterceptList[j])}")
-                #print(f"slopeREQ:{abs(1/ slopeList[i]-1 /slopeList[j])}")
-                if(abs(xInterceptList[i]-xInterceptList[j])< 10000 and abs(1/ slopeList[i]-1 /slopeList[j]) < 1):
-                    
+                
+                interceptDist = abs(xInterceptList[i]-xInterceptList[j])
+                slopeDiff = abs((1/ slopeList[i]) - (1/slopeList[j]))
+               
+                if(interceptDist > 100 and interceptDist< 10000 and slopeDiff< 1):
                     xPoint = ((slopeList[i] * xInterceptList[i]) - (slopeList[j] * xInterceptList[j]))/(slopeList[i]-slopeList[j])
                     yPoint = slopeList[i]*(xPoint - xInterceptList[i]) + 2160
                     
-                    # avgSlope = (slopeList[i]+ slopeList[j])/2
-                    # avgInterecept = (xInterceptList[i]+xInterceptList[j])/2
-                    lane1 = [xInterceptList[i], 2160, xPoint,yPoint]
-                    lane2 = [xInterceptList[j], 2160, xPoint,yPoint]
-                    addedlanes = [lane1,lane2]
-                    #print (f"thiasdfee:{(slopeList[i] * xInterceptList[i]) - slopeList[j] * xInterceptList[j]}")
-                    lanes.append(addedlanes)
+                  
+                    line1 = [xInterceptList[i], 2160, xPoint,yPoint]
+                    line2 = [xInterceptList[j], 2160, xPoint,yPoint]
+                    lane = [line1,line2]
 
+                    lanes.append(lane)
 
-            #lanes.append(lane)
-
-            #
-
-            # if (yPoint> -500 and yPoint< 1080):
-            #     avgInterceptX = (xInterceptList[i] + xInterceptList[j])/2
-            #     lane = [xPoint.item(), avgInterceptX.item(), yPoint.item(), 1080.00]
-            #     lanes.append(lane)
 
     return lanes
 
-def pick_lane(lanes):
-    maxDiff = 0
-    for addedLanes in lanes:
-        diff = abs(addedLanes[0][0]  - addedLanes[1][0])
-        if (maxDiff < diff):
-            maxDiff = diff
-            pickedLane = addedLanes
-    #print(f"picked: {pickedLane}")
-    return pickedLane
-
 def draw_lanes(img,lanes,color = (255, 0, 0)):
-    for addedLanes in lanes:
-        color = (randrange(255),randrange(255),randrange(255))
-        for lane in addedLanes:
-            
-            x1, y1, x2, y2 = lane
-            #print ("type(x1)")
-            #print (lane)
+
+    '''
+    takes an image and a list of lanes as inputs and returns an image with the lanes drawn on it. Each lane should be a different color
+    parameters:
+        img: the image to process
+        lanes: the list of lanes to draw
+
+    '''
+
+    for lane in lanes:
+        for line in lane:
+            x1, y1, x2, y2 = line
+            color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
             cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), color, 6)
     return img
 
 def draw_Single_lane(img,lanes,color = (255, 0, 0)):
-    color = (randrange(255),randrange(255),randrange(255))
+    #color = (randrange(255),randrange(255),randrange(255))
     for lane in lanes:
         
         x1, y1, x2, y2 = lane
-        #print ("type(x1)")
-        #print (lane)
+    # print ("type(x1)")
+    #  print (lane)
         cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), color, 6)
     return img
--- lane_following.py
+++ lane_following.py
@@ -1,32 +1,80 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from random import randrange
+import random
 import lane_detection
 
+
+def pick_lane(lanes):
+    '''
+    helper function for get_lane_center
+    finds the closet lane from a list of lanes
+    
+    '''
+
+    maxDiff = 0
+    for addedLanes in lanes:
+        diff = abs(addedLanes[0][0]  - addedLanes[1][0])
+        if (maxDiff < diff):
+            maxDiff = diff
+            pickedLane = addedLanes
+    
+    return pickedLane
+
+
 def get_lane_center(lanes):
-    center_intercept = (lanes[0][0]+lanes[1][0])/2
-    x1, y1, x2, y2 = lanes[0]
+    '''
+    takes a list of lanes as an input and returns the center of the closest lane and its slope
+
+    parameters:
+        lanes: the list of lanes to process
+
+    
+    '''
+
+    clane = pick_lane(lanes)
+
+
+    center = (clane[0][0]+clane[1][0])/2
+
+    x1, y1, x2, y2 = clane[0]
+
     slope1 = (y1-y2)/(x1-x2)
-    x1, y1, x2, y2 = lanes[1]
+
+    x1, y1, x2, y2 = clane[1]
+
     slope2 = (y1-y2)/(x1-x2)
-    center_slope = (slope1+slope2)/2
-    return center_intercept, center_slope
+
+    slope = (slope1+slope2)/2
+
+    
+
+    return center, slope
+
 
 def recommend_direction(center, slope):
+    '''
+    takes the center of the closest lane and its slope as inputs and returns a direction
+
+
+    parameters:
+        center: the center of the closest lane
+        slope: the slope of the closest lane
     
-    halfOfRes = 1920/2
+    
+    '''
+
+    halfOfRes = 4096/2
     if center == halfOfRes:
         direction = "forward"
-    elif center > halfOfRes:# more than halfway
-        print("strafe right")
+    elif center > halfOfRes: # more than halfway
         direction = "right"
     else:
-        print("strafe left")
         direction = "left"
+
     if 1/slope > 0:
         print("turn right")
     if 1/slope < 0:
         print("turn Left")
+   
     return direction
-    
