51faf7b2b3e7ae4db88f56f0c044e62d6511badd
jzyzak <joshuazyzak@gmail.com>
2023-07-31 15:16:14 -0400

    video frame things

--- lane_following.py
+++ lane_following.py
@@ -5,9 +5,6 @@ import matplotlib.pyplot as plt
 #from dt_apriltags import Detector
 from lane_detection import *
 import matplotlib.cm as cm
-from PIDcontrols import *
-from statistics import median
-
 
 def get_lane_center(lanes):
     center_intercept = (lanes[0][0]+lanes[1][0])/2
@@ -25,8 +22,35 @@ def draw_center_lane(img, center_intercept, center_slope, xPoint = 0, yPoint = 0
     cv2.line(img, (int(center_intercept), imgPixelHeight), (int(xPoint), int(yPoint)), (0,0,255), 6)
     return img
 
+def recommend_direction(center, slope):
+    
+    halfOfRes = 1920/2
+    HorizontalDiff = halfOfRes-center
+    centerTolerance = 2.5
+    if abs(HorizontalDiff) < centerTolerance:
+        direction = "Go Forward!"
+    elif HorizontalDiff > 0:# more than halfway
+        #print("strafe right")
+        direction = f"Strafe Left by {HorizontalDiff}"
+    else:
+        #print("strafe left")
+        direction = f"Strafe Right by {HorizontalDiff}"
+
+    AproxAUVAngle = 90 - angle_between_lines(slope, 0)  
+    # get the approx angle of the auv with the line by calculating the slope of the 
+    #center line with a horizontal line relative to the rov
+    if slope > 0:
+        AproxAUVAngle = -AproxAUVAngle
+        direction += f" turn Left by: {AproxAUVAngle} degrees"
+        pass#print("turn right")
+    if slope < 0:
+        direction += f" turn Right by: {AproxAUVAngle} degrees"
+        pass#print("turn Left")
+    return direction
+    
+"""
 def low_pass_filter_moving_median(input_data, window_size):
-    """
+
     Apply a first-order low-pass filter using the Moving Median method to the input data.
 
     Parameters:
@@ -35,7 +59,7 @@ def low_pass_filter_moving_median(input_data, window_size):
 
     Returns:
         list: Filtered output data.
-    """
+    
     half_window = window_size // 2
     filtered_data = []
 
@@ -104,21 +128,22 @@ def do_both(horizontal_control, heading_control):
     return combined_vectors
 
 def run_motors_timed(mav_connection, seconds: int, motor_settings: list) -> None:
-    """
+    
     Run the motors for a set time
     :param mav_connection: The mavlink connection
     :param time: The time to run the motors
     :param motor_settings: The motor settings, a list of 6 values -100 to 100
     :return: None
-    """
+
     start_time = time.time()
     while time.time()-start_time < seconds:
         for i in range(len(motor_settings)):
             test_motor(mav_connection=mav_connection, motor_id=i, power=motor_settings[i])
         time.sleep(0.2)
 
+
+
 def recommend_direction(center, slope, horizontal_pid, heading_pid):
-    
     halfOfRes = 1920/2
     HorizontalDiff = halfOfRes-center
     centerTolerance = 2.5
@@ -131,7 +156,7 @@ def recommend_direction(center, slope, horizontal_pid, heading_pid):
         #print("strafe left")
         direction = f"Strafe Right by {HorizontalDiff}"
 
-    horizontal_magnitudes = set_horizontal_control(HorizontalDiff, PIDoutputPosition(HorizontalDiff, horizontal_pid))
+    #horizontal_magnitudes = set_horizontal_control(HorizontalDiff, PIDoutputPosition(HorizontalDiff, horizontal_pid))
 
     AproxAUVAngle = 90 - angle_between_lines(slope, 0)  
     # get the approx angle of the auv with the line by calculating the slope of the 
@@ -144,8 +169,9 @@ def recommend_direction(center, slope, horizontal_pid, heading_pid):
         direction += f" turn Right by: {AproxAUVAngle} degrees"
         pass#print("turn Left")
 
-    heading_magnitudes = set_heading_control(AproxAUVAngle, heading_pid)
+    #heading_magnitudes = set_heading_control(AproxAUVAngle, heading_pid)
 
-    thruster_magnitudes = do_both(horizontal_magnitudes, heading_magnitudes)
+    #thruster_magnitudes = do_both(horizontal_magnitudes, heading_magnitudes)
 
-    return direction, thruster_magnitudes
+    return direction
+"""
